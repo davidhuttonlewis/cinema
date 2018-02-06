d0ce8cff60af22279e40c8dabfb257b6c5d642c9
Admin <Admin@STUDENT03>
2018-02-06 13:15:30 +0000

    Second Exercise

--- cinema/src/main/java/davidCinema/cinema/Main.java
+++ cinema/src/main/java/davidCinema/cinema/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Service service1 = new Service();
-		
+
 		service1.setUpMovies();
 		System.out.println(service1.retiveMovie(1));
 		service1.addMovie("Lion King", "Something", "E");
 		System.out.println(service1.retiveMovie(4));
-		
+
+		System.out.println(service1.convertToJson(2));
+
 	}
 
 }
