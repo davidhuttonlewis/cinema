932302b1cb70c044015005cdd256c6ef19cb041d
Admin <Admin@STUDENT03>
2018-02-06 11:33:42 +0000

    Have made changes based on John L's feed back

--- cinema/src/main/java/davidCinema/cinema/Service.java
+++ cinema/src/main/java/davidCinema/cinema/Service.java
@@ -1,36 +1,30 @@
 package davidCinema.cinema;
 
-
 import java.util.HashMap;
 
 public class Service {
-	
-	static int idCounter = 3;
-	//ArrayList<Movie> arrayList = new ArrayList<Movie>();
-	private HashMap <Integer, Movie> movieMap = new HashMap<>();
-	
-public void setUpMovies() {
-	
-	
-	movieMap.put(1, new Movie("Stargate", "Sci-fi", "A12"));
-	movieMap.put(2, new Movie("Transformers", "Death of my Childhood", "E"));
-	movieMap.put(3, new Movie("Some Movie", "Don't care", "18"));
-	
-}
 
-public Movie retiveMovie(int key) {
-	
-	return movieMap.get(key);
+	static int idCounter = 0;
+	// ArrayList<Movie> arrayList = new ArrayList<Movie>();
+	private HashMap<Integer, Movie> movieMap = new HashMap<>();
 
-}
+	public void setUpMovies() {
 
-public void addMovie(String title, String genre, String ageRating) {
-	
-	idCounter += 1;
-	movieMap.put(idCounter, new Movie(title, genre, ageRating));
-	
-}
+		addMovie("Stargate", "Sci-fi", "A12");
+		addMovie("Transformers", "Death of my Childhood", "E");
+		addMovie("Some Movie", "Don't care", "18");
+	}
 
+	public Movie retiveMovie(int key) {
 
+		return movieMap.get(key);
+	}
+
+	public void addMovie(String title, String genre, String ageRating) {
+
+		idCounter += 1;
+		movieMap.put(idCounter, new Movie(title, genre, ageRating));
+
+	}
 
 }
